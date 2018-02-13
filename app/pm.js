9d3dfdfe335088e08a28a36614700c14b1df3954
Anders Bakken <agbakken@gmail.com>
2018-02-13 14:07:46 -0800

    More work.

---
+++ app/pm.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+/* global require, process */
+module.exports = () => {
+    const options = require('@jhanssen/options')('pm');
+    const transform = require('./transform');
+    const generator = require('./generator');
+
+    const prompt = require('prompt');
+    prompt.message = '';
+    prompt.start();
+
+    function read(name, opts)
+    {
+        return new Promise((resolve, reject) => {
+            const value = options(name);
+            if (value) {
+                resolve(value);
+                return;
+            }
+
+            var schema = { properties: { } };
+            schema.properties[name] = {
+                required: true
+            };
+            if (opts) {
+                for (let key in opts) {
+                    schema.properties[name][key] = opts[key];
+                }
+            }
+            prompt.get(schema, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result[name]);
+                }
+            });
+        });
+    };
+
+    var params = { password: undefined, components: {} };
+
+    read('user').then(result => {
+        params.components.user = result;
+        return read('host');
+    }).then(result => {
+        params.components.host = result;
+        return read('revision', { default: "1", pattern: /^[0-9]+$/ });
+    }).then(result => {
+        params.components.revision = parseInt(result);
+        return read('password', { hidden: true });
+    }).then(result => {
+        params.password = result;
+        return read('length', { default: "12", pattern: /^[0-9]+$/ });
+    }).then(result => {
+        params.length = parseInt(result);
+        return read('transform', { default: 'printable94' });
+    }).then(result => {
+        const binary = generator(params);
+        const out = transform(result)(binary, params.length);
+        console.log('password:\n', out);
+        // console.log('user', user, 'host', host, 'password', password);
+    }).catch(error => {
+        console.error('Got error', error);
+        process.exit(1);
+    });
+};
