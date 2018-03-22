5072918a73c124b5f7fbe71ae6dec0476cc11317
Anders Bakken <agbakken@gmail.com>
2018-03-21 21:17:36 -0700

    Support no cipher by passing --no-cipher to pm

--- app/blob.js
+++ app/blob.js
@@ -3,8 +3,10 @@
 const crypto = require('crypto');
 const scrypt = require('js-scrypt');
 
-const CurrentVersion = 1;
+const Version = 100;
+const NoCipherVersion = 2;
 const algorithm = 'aes-256-gcm';
+const options = require('@jhanssen/options')('pm');
 
 function deriveKey(password, salt)
 {
@@ -21,7 +23,7 @@ module.exports = {
         if (buffer.length <= 4 + 16 + 16 + 16)
             return undefined;
         const version = buffer.readUInt32BE(0);
-        if (version != CurrentVersion)
+        if (version != Version && version != NoCipherVersion)
             return undefined;
         return {
             version: version,
@@ -37,24 +39,35 @@ module.exports = {
         return Buffer.concat([versionBuffer, object.scryptSalt, object.iv, object.tag, object.cipherText]).toString('base64');
     },
     encrypt: function (password, plainText) {
-        var blob = {
-            version: CurrentVersion,
-            scryptSalt: crypto.randomBytes(16),
-            iv: crypto.randomBytes(16)
-        };
-        const cipher = crypto.createCipheriv(algorithm, deriveKey(password, blob.scryptSalt), blob.iv);
-        blob.cipherText = cipher.update(plainText, 'utf8');
-        cipher.final();
-        blob.tag = cipher.getAuthTag();
+        var blob = {};
+        if (options("cipher") === false) {
+            blob.version = NoCipherVersion;
+            blob.cipherText = Buffer.from(plainText, 'utf8');
+            blob.scryptSalt = Buffer.alloc(16);
+            blob.iv = Buffer.alloc(16);
+            blob.tag = Buffer.alloc(16);
+        } else {
+            blob.version = Version;
+            blob.scryptSalt = crypto.randomBytes(16);
+            blob.iv = crypto.randomBytes(16);
+            const cipher = crypto.createCipheriv(algorithm, deriveKey(password, blob.scryptSalt), blob.iv);
+            blob.cipherText = cipher.update(plainText, 'utf8');
+            cipher.final();
+            blob.tag = cipher.getAuthTag();
+        }
         return blob;
     },
     decrypt: function (password, blob) {
-        let entries = [];
-
-        const decipher = crypto.createDecipheriv(algorithm, deriveKey(password, blob.scryptSalt), blob.iv);
-        decipher.setAuthTag(blob.tag);
-        let dec = decipher.update(blob.cipherText, 'utf8');
-        decipher.final();
-        return dec;
+        if (blob.version == Version) {
+            const decipher = crypto.createDecipheriv(algorithm, deriveKey(password, blob.scryptSalt), blob.iv);
+            decipher.setAuthTag(blob.tag);
+            let dec = decipher.update(blob.cipherText, 'utf8');
+            decipher.final();
+            return dec;
+        } else if (blob.version == NoCipherVersion) {
+            return blob.cipherText.toString('utf8');
+        } else {
+            throw new Error("Bad blob version " + blob.version);
+        }
     }
 };
